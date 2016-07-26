a8777769d031d03b2a461258e587fdfc0b30efb0
youmu178 <youmu178@sina.com>
2016-07-26 11:17:32 +0800

    init

--- app/src/main/java/com/youzh/arcprogressbar/MainActivity.java
+++ app/src/main/java/com/youzh/arcprogressbar/MainActivity.java
@@ -3,6 +3,10 @@ package com.youzh.arcprogressbar;
 import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.youzh.ArcProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,17 +17,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mArcProgressBar = (ArcProgressBar) findViewById(R.id.arcProgressBar);
-
-        ValueAnimator valueAnimator =ValueAnimator.ofInt(0, 75);
-        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+        Button btRestart = (Button) findViewById(R.id.button);
+        btRestart.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                animation.getAnimatedValue();
-                mArcProgressBar.setProgress((int) animation.getAnimatedValue());
+            public void onClick(View view) {
+                mArcProgressBar.restart();
             }
         });
-        valueAnimator.setDuration(5000);
-        valueAnimator.start();
+        Button btStart = (Button) findViewById(R.id.button2);
+        btStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ValueAnimator valueAnimator =ValueAnimator.ofInt(0, 100);
+                valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        mArcProgressBar.setProgress((int) animation.getAnimatedValue());
+                    }
+                });
+                valueAnimator.setDuration(5000);
+                valueAnimator.start();
+            }
+        });
 
     }
 }
